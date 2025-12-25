6db7cfc2fffbf9eb1370f18dc0a4253f5d78f5e6
Aditya Ahfazh Firmansyah <117560935+Alphby48@users.noreply.github.com>
2025-12-26 03:09:13 +0700

    Comment out unused component imports
    Comment out imports for App and LandingPage components

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
-import LandingPage from "./App2.jsx";
+//import App from "./App.jsx";
+//import LandingPage from "./App2.jsx";
 import LandingPages from "./home.jsx";
 
 createRoot(document.getElementById("root")).render(
